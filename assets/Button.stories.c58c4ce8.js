2aa93bdee8a0e150da7e93ff0d22f2edbb28224f
Allens <philallen84@gmail.com>
2023-01-06 09:16:42 +0000

    inputs

---
+++ assets/Button.stories.c58c4ce8.js
@@ -0,0 +1,11 @@
+var q=Object.defineProperty;var x=(B,s)=>q(B,"name",{value:s,configurable:!0});import{S as w,i as F,s as L,r as l,u as r,v as u,a as n,q as S,d as o}from"./index.7d4f0ec3.js";function k(B){let s,d,p,y,b,v,i,c,a,C,f,_,m;return{c(){s=l("button"),s.textContent="Primary",d=r(),p=l("button"),p.textContent="Secondary",y=r(),b=l("button"),b.textContent="Warning",v=r(),i=l("button"),i.textContent="Disabled",c=r(),a=l("button"),a.textContent="Small primary",C=r(),f=l("button"),f.textContent="Small secondary",_=r(),m=l("button"),m.textContent="Small disabled",u(s,"class","primaryButton svelte-1bxzn01"),u(p,"class","secondaryButton svelte-1bxzn01"),u(b,"class","warningButton svelte-1bxzn01"),u(i,"class","primaryButton svelte-1bxzn01"),i.disabled=!0,u(a,"class","primarySmallButton svelte-1bxzn01"),u(f,"class","secondarySmallButton svelte-1bxzn01"),u(m,"class","primarySmallButton svelte-1bxzn01"),m.disabled=!0},m(t,e){n(t,s,e),n(t,d,e),n(t,p,e),n(t,y,e),n(t,b,e),n(t,v,e),n(t,i,e),n(t,c,e),n(t,a,e),n(t,C,e),n(t,f,e),n(t,_,e),n(t,m,e)},p:S,i:S,o:S,d(t){t&&o(s),t&&o(d),t&&o(p),t&&o(y),t&&o(b),t&&o(v),t&&o(i),t&&o(c),t&&o(a),t&&o(C),t&&o(f),t&&o(_),t&&o(m)}}}x(k,"create_fragment");class z extends w{constructor(s){super(),F(this,s,null,k,L,{})}}x(z,"Button$1");z.__docgen={version:3,name:"Button.svelte",data:[],computed:[],methods:[],components:[],description:null,keywords:[],events:[],slots:[],refs:[]};const M={parameters:{storySource:{source:`import ButtonComponent from "./Button.svelte";
+
+export default {
+  title: "Composites/Form/Button",
+};
+
+export const Button = () => ({
+  Component: ButtonComponent,
+});
+`,locationsMap:{button:{startLoc:{col:22,line:7},endLoc:{col:2,line:9},startBody:{col:22,line:7},endBody:{col:2,line:9}}}}},title:"Composites/Form/Button"},O=x(()=>({Component:z}),"Button"),P=["Button"];export{O as Button,P as __namedExportsOrder,M as default};
+//# sourceMappingURL=Button.stories.c58c4ce8.js.map
