c7162c997dfbcf169d153998c7cee3d26d1105e3
Allen <philallen84@gmail.com>
2025-04-07 13:26:14 +0100

    doug 4

---
+++ assets/IconButton-BBhNww2m.js
@@ -0,0 +1,10 @@
+import{j as t}from"./jsx-runtime-BkBIjjFT.js";import{useMDXComponents as e}from"./index-CNwVr1BN.js";import{M as a,C as r,a as m}from"./index-BQNdGnmF.js";import{I as c,B as i,D as p,C as d}from"./IconButton.stories-Bn4YHF-1.js";import"./index-KmaxkdDD.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-DaCOH5i2.js";import"./index-CVhlOzZO.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./index-VIbBARS4.js";import"./index-qhhwF6MA.js";import"./index-BQlaamZD.js";import"./IconButton-CAwCZCHp.js";import"./index-DT8u_GqS.js";function s(o){const n={h1:"h1",h2:"h2",h3:"h3",...e(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(a,{of:c}),`
+`,t.jsx(n.h1,{id:"iconbutton",children:"IconButton"}),`
+`,t.jsx(n.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(r,{of:i}),`
+`,t.jsx(m,{of:i}),`
+`,t.jsx(n.h2,{id:"variants",children:"Variants"}),`
+`,t.jsx(n.h3,{id:"default",children:"Default"}),`
+`,t.jsx(r,{of:p}),`
+`,t.jsx(n.h3,{id:"contained",children:"Contained"}),`
+`,t.jsx(r,{of:d})]})}function g(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(s,{...o})}):s(o)}export{g as default};
---
+++ assets/TextInputV2-Th1PvGfF.js
@@ -0,0 +1,21 @@
+import{j as t}from"./jsx-runtime-BkBIjjFT.js";import{useMDXComponents as e}from"./index-CNwVr1BN.js";import{M as m,C as o,a}from"./index-BQNdGnmF.js";import{T as p,B as n,W as h,a as l,D as d,I as x,b as c,c as j,d as f}from"./TextInputV2.stories-3GHYpKdJ.js";import"./index-KmaxkdDD.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-DaCOH5i2.js";import"./index-CVhlOzZO.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./index-D0csYWE-.js";import"./index-qhhwF6MA.js";import"./index-BQlaamZD.js";import"./TextField-C4j0Nh0A.js";import"./utils-l5KLAjAw.js";import"./chain-Z-ySmOka.js";import"./Form-DDhzfXRW.js";import"./useFocusRing-xuvxHm_5.js";import"./platform-COItUIJl.js";import"./Label-DGdLAgDC.js";import"./useLabels-BOZ2JF2m.js";import"./Input-DQodsJoB.js";import"./Text-DsbvCy5R.js";import"./useTextField-BF95WJiu.js";import"./useControlledState-BCPvzKxG.js";import"./TextInputV2.styled-B04IJZ78.js";import"./index-DT8u_GqS.js";import"./InputAlerts-BgCOBeSp.js";import"./InlineAlert-BV23CUO-.js";import"./Typography-wg1R8lvT.js";import"./index-VIbBARS4.js";import"./InputLabelTopV2-Cca_2KhX.js";import"./InputLabelTopTooltip-C0KDMlGy.js";import"./TooltipTrigger-DTxXleLQ.js";import"./floating-ui.react-DZDdj-Od.js";import"./Button-lwQFUGeW.js";import"./usePress-DcYN71RK.js";import"./OverlayContext-CQkNAI0D.js";import"./OuterFieldV2-CvQ6t54L.js";import"./CharactersCounter-DBhpAW1O.js";function s(r){const i={h1:"h1",h2:"h2",h3:"h3",p:"p",...e(),...r.components};return t.jsxs(t.Fragment,{children:[t.jsx(m,{of:p}),`
+`,t.jsx(i.h1,{id:"textinputv2",children:"TextInputV2"}),`
+`,t.jsx(i.h2,{id:"properties",children:"Properties"}),`
+`,t.jsxs(i.p,{children:["For a complete list of available props, please refer to the React Aria ",t.jsx("a",{href:"https://react-spectrum.adobe.com/react-aria/TextField.html#props",className:"boi-mdx-link",target:"_blank",children:"documentation"}),"."]}),`
+`,t.jsx(o,{of:n}),`
+`,t.jsx(a,{of:n}),`
+`,t.jsx(i.h2,{id:"variants",children:"Variants"}),`
+`,t.jsx(i.h3,{id:"with-label",children:"With label"}),`
+`,t.jsx(o,{of:h}),`
+`,t.jsx(i.h3,{id:"with-hint",children:"With hint"}),`
+`,t.jsx(o,{of:l}),`
+`,t.jsx(i.h3,{id:"disabled",children:"Disabled"}),`
+`,t.jsx(o,{of:d}),`
+`,t.jsx(i.h3,{id:"invalid",children:"Invalid"}),`
+`,t.jsx(o,{of:x}),`
+`,t.jsx(i.h3,{id:"with-limit",children:"With limit"}),`
+`,t.jsx(o,{of:c}),`
+`,t.jsx(i.h3,{id:"with-start-slot",children:"With start slot"}),`
+`,t.jsx(o,{of:j}),`
+`,t.jsx(i.h3,{id:"with-end-slot",children:"With end slot"}),`
+`,t.jsx(o,{of:f})]})}function rt(r={}){const{wrapper:i}={...e(),...r.components};return i?t.jsx(i,{...r,children:t.jsx(s,{...r})}):s(r)}export{rt as default};
