a6685bb6385f4c9a3ca024d3dc9f63535a72ad20
Allen <philallen84@gmail.com>
2025-04-07 14:20:18 +0100

    doug 4

---
+++ assets/Button-rOq-PJyJ.js
@@ -0,0 +1,16 @@
+import{j as n}from"./jsx-runtime-BkBIjjFT.js";import{useMDXComponents as e}from"./index-CNwVr1BN.js";import{M as a,C as i,a as d}from"./index-Bfa_Y5mY.js";import{B as c,a as s,S as h,D as m,L as x,b as p,W as j}from"./Button.stories-Cho2gh3O.js";import"./index-KmaxkdDD.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-Mxng_W-2.js";import"./index-CVhlOzZO.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./index-qhhwF6MA.js";import"./index-BQlaamZD.js";import"./index-VIbBARS4.js";import"./Button-DdoqnsX4.js";import"./index-DT8u_GqS.js";import"./Typography-wg1R8lvT.js";import"./Spinner-oUHXJT3k.js";function r(t){const o={h1:"h1",h2:"h2",h3:"h3",...e(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(a,{of:c}),`
+`,n.jsx(o.h1,{id:"button",children:"Button"}),`
+`,n.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,n.jsx(i,{of:s}),`
+`,n.jsx(d,{of:s}),`
+`,n.jsx(o.h2,{id:"variants",children:"Variants"}),`
+`,n.jsx(o.h3,{id:"secondary",children:"Secondary"}),`
+`,n.jsx(i,{of:h}),`
+`,n.jsx(o.h3,{id:"disabled",children:"Disabled"}),`
+`,n.jsx(i,{of:m}),`
+`,n.jsx(o.h3,{id:"loading",children:"Loading"}),`
+`,n.jsx(i,{of:x}),`
+`,n.jsx(o.h3,{id:"stacked",children:"Stacked"}),`
+`,n.jsx(i,{of:p}),`
+`,n.jsx(o.h3,{id:"with-icon",children:"With icon"}),`
+`,n.jsx(i,{of:j})]})}function _(t={}){const{wrapper:o}={...e(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(r,{...t})}):r(t)}export{_ as default};
---
+++ assets/CheckboxV2-C3vSB9qJ.js
@@ -0,0 +1,15 @@
+import{j as t}from"./jsx-runtime-BkBIjjFT.js";import{useMDXComponents as s}from"./index-CNwVr1BN.js";import{M as a,C as r,a as m}from"./index-Bfa_Y5mY.js";import{C as p,B as e,W as h,D as c,I as d,a as l}from"./CheckboxV2.stories-Du3eD0Dn.js";import"./index-KmaxkdDD.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-Mxng_W-2.js";import"./index-CVhlOzZO.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./index-D0csYWE-.js";import"./CheckboxV2-Btkfcs8x.js";import"./Checkbox-fbKcaDqu.js";import"./RSPContexts-DVGGaWhm.js";import"./utils-l5KLAjAw.js";import"./chain-Z-ySmOka.js";import"./Form-DDhzfXRW.js";import"./useFocusRing-xuvxHm_5.js";import"./platform-COItUIJl.js";import"./Label-DGdLAgDC.js";import"./useLabels-BOZ2JF2m.js";import"./Text-DsbvCy5R.js";import"./usePress-DcYN71RK.js";import"./useControlledState-BCPvzKxG.js";import"./VisuallyHidden-to5K4EiD.js";import"./index-qhhwF6MA.js";import"./index-DT8u_GqS.js";import"./InputLabelRightV2-BQF2rjuU.js";import"./Typography-wg1R8lvT.js";import"./index-VIbBARS4.js";import"./index-BQlaamZD.js";function n(o){const i={h1:"h1",h2:"h2",h3:"h3",p:"p",...s(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(a,{of:p}),`
+`,t.jsx(i.h1,{id:"checkboxv2",children:"CheckboxV2"}),`
+`,t.jsx(i.h2,{id:"properties",children:"Properties"}),`
+`,t.jsxs(i.p,{children:["For a complete list of available props, please refer to the React Aria ",t.jsx("a",{href:"https://react-spectrum.adobe.com/react-aria/Checkbox.html#props",className:"boi-mdx-link",target:"_blank",children:"documentation"}),"."]}),`
+`,t.jsx(r,{of:e}),`
+`,t.jsx(m,{of:e}),`
+`,t.jsx(i.h2,{id:"variants",children:"Variants"}),`
+`,t.jsx(i.h3,{id:"with-label",children:"With label"}),`
+`,t.jsx(r,{of:h}),`
+`,t.jsx(i.h3,{id:"disabled",children:"Disabled"}),`
+`,t.jsx(r,{of:c}),`
+`,t.jsx(i.h3,{id:"invalid",children:"Invalid"}),`
+`,t.jsx(r,{of:d}),`
+`,t.jsx(i.h3,{id:"indeterminate",children:"Indeterminate"}),`
+`,t.jsx(r,{of:l})]})}function J(o={}){const{wrapper:i}={...s(),...o.components};return i?t.jsx(i,{...o,children:t.jsx(n,{...o})}):n(o)}export{J as default};
